ae1b03c3e6ffdfb5c261285a2b52972bf65559ae
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2020-10-22 23:47:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min = nul
+  min = null
   name_hash.each do |element, key|
-    if(min == nul)
+    if(min == null)
       min = key 
     end 
   end
