c24751eaf5217112d3b3fb54155e2be554b2e8ee
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2020-10-22 23:45:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,8 @@
 def key_for_min_value(name_hash)
   min = name_hash.first 
   name_hash.each do |element, key|
-    if(min > element)
-      min = element
+    if(min > key)
+      min = key 
     end 
   end
   return name_hash[min]
